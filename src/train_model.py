8b93599d81cf1aaf8792388612c6b550a737fbe6
Dmytro Maksymchuk <d.maksymchuk@student.tudelft.nl>
2025-05-05 19:22:18 +0200

    Saving CountVectorizer

--- src/train_model.py
+++ src/train_model.py
@@ -1,4 +1,6 @@
 import argparse
+import pickle
+
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
@@ -20,25 +22,28 @@ def main():
     parser.add_argument('--dataset', type=str, default="../datasets/a1_RestaurantReviews_HistoricDump.tsv",
                         help="Path to the dataset (TSV file).")
     parser.add_argument('--train_all', action='store_true', help="Train on the full dataset without evaluation.")
-    parser.add_argument('--output', type=str, default="../output/c2_Classifier_Sentiment_Model.pkl",
+    parser.add_argument('--output', type=str, default="../output/",
                         help="Path to save the trained model.")
 
     args = parser.parse_args()
 
     messages = pd.read_csv(args.dataset, delimiter='\t', quoting=3)
-    X = libml._preprocess(messages).toarray()
+    X, cv = libml._preprocess(messages)
     y = messages.iloc[:, -1].values
+
+    bow_path = args.output + 'c1_BoW_Sentiment_Model.pkl'
+    pickle.dump(cv, open(bow_path, "wb"))
 
     if args.train_all:
         # Train on the full dataset
         classifier = GaussianNB()
         classifier.fit(X, y)
-        joblib.dump(classifier, args.output)
+        joblib.dump(classifier, args.output + 'c2_Classifier_Sentiment_Model.pkl')
     else:
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=20)
         classifier = GaussianNB()
         classifier.fit(X_train, y_train)
-        joblib.dump(classifier, args.output)
+        joblib.dump(classifier, args.output + 'c2_Classifier_Sentiment_Model.pkl')
 
         # Evaluate the performance of the model
         eval_performance(classifier, X_test, y_test)
